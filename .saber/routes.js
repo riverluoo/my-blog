c395f207ace9404f1b4aecc0248dc9657321e52b
wangyangyang <769789170@qq.com>
2019-05-10 14:54:45 +0800

    Update

--- .saber/routes.js
+++ .saber/routes.js
@@ -1,37 +1,253 @@
 export default [
       {
-              path: "/posts/my-example-post.html",
+              path: "/posts/concurrent/Java-base.html",
               meta: {
-                __relative: '_posts/my-example-post.md',
-                __pageId: '24a109e6'
+                __relative: '_posts/concurrent/Java-base.md',
+                __pageId: '1afeac08'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-my-example-post-md" */ "/root/src/my-blog/pages/_posts/my-example-post.md?saberPage=24a109e6")
+                return import(/* webpackChunkName: "page--_posts-concurrent-Java-base-md" */ "/root/src/my-blog/pages/_posts/concurrent/Java-base.md?saberPage=1afeac08")
                 
               }
             },
 {
-              path: "/posts/super-long-article.html",
+              path: "/posts/concurrent/Java-collection.html",
               meta: {
-                __relative: '_posts/super-long-article.md',
-                __pageId: '164e056e'
+                __relative: '_posts/concurrent/Java-collection.md',
+                __pageId: '27f9d5e2'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-super-long-article-md" */ "/root/src/my-blog/pages/_posts/super-long-article.md?saberPage=164e056e")
+                return import(/* webpackChunkName: "page--_posts-concurrent-Java-collection-md" */ "/root/src/my-blog/pages/_posts/concurrent/Java-collection.md?saberPage=27f9d5e2")
                 
               }
             },
 {
-              path: "/posts/super-short-article.html",
+              path: "/posts/concurrent/Java-concurrent.html",
               meta: {
-                __relative: '_posts/super-short-article.md',
-                __pageId: 'b548fe52'
+                __relative: '_posts/concurrent/Java-concurrent.md',
+                __pageId: '6904db56'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-super-short-article-md" */ "/root/src/my-blog/pages/_posts/super-short-article.md?saberPage=b548fe52")
+                return import(/* webpackChunkName: "page--_posts-concurrent-Java-concurrent-md" */ "/root/src/my-blog/pages/_posts/concurrent/Java-concurrent.md?saberPage=6904db56")
+                
+              }
+            },
+{
+              path: "/posts/concurrent/Java-thread.html",
+              meta: {
+                __relative: '_posts/concurrent/Java-thread.md',
+                __pageId: '66d9c223'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-concurrent-Java-thread-md" */ "/root/src/my-blog/pages/_posts/concurrent/Java-thread.md?saberPage=66d9c223")
+                
+              }
+            },
+{
+              path: "/posts/concurrent/Java-vm.html",
+              meta: {
+                __relative: '_posts/concurrent/Java-vm.md',
+                __pageId: '2f74ba56'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-concurrent-Java-vm-md" */ "/root/src/my-blog/pages/_posts/concurrent/Java-vm.md?saberPage=2f74ba56")
+                
+              }
+            },
+{
+              path: "/posts/exception/NoClassDefFoundError.html",
+              meta: {
+                __relative: '_posts/exception/NoClassDefFoundError.md',
+                __pageId: '6876caec'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-exception-NoClassDefFoundError-md" */ "/root/src/my-blog/pages/_posts/exception/NoClassDefFoundError.md?saberPage=6876caec")
+                
+              }
+            },
+{
+              path: "/posts/exception/dev.html",
+              meta: {
+                __relative: '_posts/exception/dev.md',
+                __pageId: '4ad564d5'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-exception-dev-md" */ "/root/src/my-blog/pages/_posts/exception/dev.md?saberPage=4ad564d5")
+                
+              }
+            },
+{
+              path: "/posts/middle/centos.html",
+              meta: {
+                __relative: '_posts/middle/centos.md',
+                __pageId: '5c751244'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-middle-centos-md" */ "/root/src/my-blog/pages/_posts/middle/centos.md?saberPage=5c751244")
+                
+              }
+            },
+{
+              path: "/posts/middle/git.html",
+              meta: {
+                __relative: '_posts/middle/git.md',
+                __pageId: '09ad3408'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-middle-git-md" */ "/root/src/my-blog/pages/_posts/middle/git.md?saberPage=09ad3408")
+                
+              }
+            },
+{
+              path: "/posts/middle/mysql.html",
+              meta: {
+                __relative: '_posts/middle/mysql.md',
+                __pageId: '12d52ff8'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-middle-mysql-md" */ "/root/src/my-blog/pages/_posts/middle/mysql.md?saberPage=12d52ff8")
+                
+              }
+            },
+{
+              path: "/posts/middle/nginx.html",
+              meta: {
+                __relative: '_posts/middle/nginx.md',
+                __pageId: '9d5e2c00'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-middle-nginx-md" */ "/root/src/my-blog/pages/_posts/middle/nginx.md?saberPage=9d5e2c00")
+                
+              }
+            },
+{
+              path: "/posts/middle/redis.html",
+              meta: {
+                __relative: '_posts/middle/redis.md',
+                __pageId: '438f9f02'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-middle-redis-md" */ "/root/src/my-blog/pages/_posts/middle/redis.md?saberPage=438f9f02")
+                
+              }
+            },
+{
+              path: "/posts/netty/sticky-package.html",
+              meta: {
+                __relative: '_posts/netty/sticky-package.md',
+                __pageId: '37801211'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-netty-sticky-package-md" */ "/root/src/my-blog/pages/_posts/netty/sticky-package.md?saberPage=37801211")
+                
+              }
+            },
+{
+              path: "/posts/spring/time-task.html",
+              meta: {
+                __relative: '_posts/spring/time-task.md',
+                __pageId: '66d5e586'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-spring-time-task-md" */ "/root/src/my-blog/pages/_posts/spring/time-task.md?saberPage=66d5e586")
+                
+              }
+            },
+{
+              path: "/posts/travel/2018-09-16-fuyang.html",
+              meta: {
+                __relative: '_posts/travel/2018-09-16-fuyang.md',
+                __pageId: '7e600099'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-travel-2018-09-16-fuyang-md" */ "/root/src/my-blog/pages/_posts/travel/2018-09-16-fuyang.md?saberPage=7e600099")
+                
+              }
+            },
+{
+              path: "/posts/travel/2018-09-24.chongming.html",
+              meta: {
+                __relative: '_posts/travel/2018-09-24.chongming.md',
+                __pageId: '0b6ce32d'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-travel-2018-09-24-chongming-md" */ "/root/src/my-blog/pages/_posts/travel/2018-09-24.chongming.md?saberPage=0b6ce32d")
+                
+              }
+            },
+{
+              path: "/posts/travel/2018-11-09-tongxiang.html",
+              meta: {
+                __relative: '_posts/travel/2018-11-09-tongxiang.md',
+                __pageId: 'ad38d0c2'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-travel-2018-11-09-tongxiang-md" */ "/root/src/my-blog/pages/_posts/travel/2018-11-09-tongxiang.md?saberPage=ad38d0c2")
+                
+              }
+            },
+{
+              path: "/posts/travel/2018-11-25-jiaxing.html",
+              meta: {
+                __relative: '_posts/travel/2018-11-25-jiaxing.md',
+                __pageId: '209f156e'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-travel-2018-11-25-jiaxing-md" */ "/root/src/my-blog/pages/_posts/travel/2018-11-25-jiaxing.md?saberPage=209f156e")
+                
+              }
+            },
+{
+              path: "/posts/travel/maotai.html",
+              meta: {
+                __relative: '_posts/travel/maotai.md',
+                __pageId: '0b6f5e94'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-travel-maotai-md" */ "/root/src/my-blog/pages/_posts/travel/maotai.md?saberPage=0b6f5e94")
+                
+              }
+            },
+{
+              path: "/posts/web/url.html",
+              meta: {
+                __relative: '_posts/web/url.md',
+                __pageId: '2b2a5a56'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts-web-url-md" */ "/root/src/my-blog/pages/_posts/web/url.md?saberPage=2b2a5a56")
+                
+              }
+            },
+{
+              path: "/posts/我的2018年.html",
+              meta: {
+                __relative: '_posts/我的2018年.md',
+                __pageId: '3e4af3e4'
+              },
+              component: function() {
+                
+                return import(/* webpackChunkName: "page--_posts---2018--md" */ "/root/src/my-blog/pages/_posts/我的2018年.md?saberPage=3e4af3e4")
                 
               }
             },
