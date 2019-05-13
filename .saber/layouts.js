704f88f5b7f79132d354408a0ba06df8d57e48f0
wangyangyang <769789170@qq.com>
2019-05-13 13:10:14 +0800

    Update run

--- .saber/layouts.js
+++ .saber/layouts.js
@@ -5,12 +5,12 @@ var layouts = {}
       layouts["default"] = layout_0
       
 
-      import layout_1 from "/Users/riverluoo/Me/my-blog/node_modules/saber-theme-minima/dist/layouts/page.vue"
-      layouts["page"] = layout_1
+      import layout_1 from "/Users/riverluoo/Me/my-blog/node_modules/saber-theme-minima/dist/layouts/post.vue"
+      layouts["post"] = layout_1
       
 
-      import layout_2 from "/Users/riverluoo/Me/my-blog/node_modules/saber-theme-minima/dist/layouts/post.vue"
-      layouts["post"] = layout_2
+      import layout_2 from "/Users/riverluoo/Me/my-blog/node_modules/saber-theme-minima/dist/layouts/page.vue"
+      layouts["page"] = layout_2
       
 
     export default layouts
